ffac2b9a56573bfca6b861641880aff06c493688
vishalpwaman123 <vishalpwaman123@gmail.com>
2024-12-22 12:40:18 +0530

    Added : Initial structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+    { path : "", component : SigninComponent },
+    { path : "signup", component : SignupComponent },
+    { path : "dashboard", loadChildren: ()=> import("./dashboard/dashboard.module").then(module => module.DashboardModule) }
+];
